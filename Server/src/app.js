5fc5e89ab1bb0bf2008ba774c1540d5390571305
PauloDamianVinci <paulodamianvinci@gmail.com>
2023-12-27 11:53:14 -0300

    fix

--- Server/src/app.js
+++ Server/src/app.js
@@ -4,27 +4,15 @@ const server = express();
 
 // Middleware para tener acceso sin seguridad (uso local):
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
-    //Preflight CORS handler
-    // if (req.method === 'OPTIONS') {
-    //     return res.status(200).json(({
-    //         body: "OK"
-    //     }))
-    // }
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
 });
-// server.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//     next();
-// });
 // Middleware para manejar formato json (body):
 server.use(express.json());
 // Middleware para anteponerle "/listador" a las rutas:
